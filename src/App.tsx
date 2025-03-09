21a20f9b51e4066fa965def9c267e92e57056fcd
muhammedshamil8 <muhammedshamil008@gmail.com>
2025-03-09 09:21:52 +0530

    #

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // Import pages
 import Welcome from "./pages/Welcome";
@@ -42,7 +42,8 @@ const App = () => (
           <Route path="/pcos" element={<AppShell><PCOSQuestionnaire /></AppShell>} />
           
           {/* Catch-all route */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Navigate to={'/'} />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
